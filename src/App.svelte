ef60c43272918e00c16dec34622882886117cc78
KoenMW <89910070+KoenMW@users.noreply.github.com>
2025-02-06 14:07:21 +0100

    chore added button to app.svelte for testing; removed main.yml

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,11 @@
+<script>
+  import Button from "./components/common/Button.svelte";
+
+  const click = () => {
+    console.log("clicked");
+  };
+</script>
+
 <body> hoi </body>
+
+<Button on:click={click} />
