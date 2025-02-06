e603608c1370ca47314675b2d2df0935b87c553f
KoenMW <89910070+KoenMW@users.noreply.github.com>
2025-02-06 13:51:48 +0100

    feat: basic button; action test

---
+++ src/components/common/Button.svelte
@@ -0,0 +1,18 @@
+<button on:click class="button">
+  <slot></slot>
+</button>
+
+<style>
+  .button {
+    border: none;
+    transition: all 1s ease-in-out;
+  }
+
+  .button:hover {
+    scale: 1.1;
+  }
+
+  .button:active {
+    scale: 0.9;
+  }
+</style>
